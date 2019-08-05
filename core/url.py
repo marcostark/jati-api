417906016fed95296dd5e8063c1179e5c9934e35
Marcos <mrcs.stark@gmail.com>
2019-08-04 22:00:44 -0300

    Implementando serializers

---
+++ core/url.py
@@ -0,0 +1,16 @@
+from django.urls import path, include
+from . import views
+
+urlpatterns = [
+# ---------------------------------------------------------------------
+
+    #path('speciality/', views.SpecialityListView.as_view()),
+
+# ---------------------------------------------------------------------
+
+    #path('apointmentstatus/', views.ApointmentStatusCreateView.as_view()),
+
+# ---------------------------------------------------------------------
+
+    #path('apointmentstatus/', views.ProfessionalListView.as_view()),
+]
