eb3f359cdbd6965b972c3c43324dff7da2d8f173
Marcos <mrcs.stark@gmail.com>
2019-08-04 22:43:33 -0300

    Configurando arquivos staticos

--- jati/urls.py
+++ jati/urls.py
@@ -13,6 +13,8 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path
 from django.urls import path, include
@@ -20,4 +22,4 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/v1/core/', include('core.urls')),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
